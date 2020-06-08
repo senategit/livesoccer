1adb5fce7d1d17afd1e184f5c0308dac9b78f35f
Nileeye1992 <43407329+Nileeye1992@users.noreply.github.com>
2020-06-08 11:05:46 +0700

    update

--- test/src/app/component/livesoccer/read-analyze/read-analyze.component.ts
+++ test/src/app/component/livesoccer/read-analyze/read-analyze.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-read-analyze',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadAnalyzeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location : Location) { }
 
   ngOnInit() {
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
